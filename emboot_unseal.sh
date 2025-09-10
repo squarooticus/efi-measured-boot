cb06394f59c798f90770e5245222c4c6ea733aa1
Kyle Rose <krose@krose.org>
2025-09-10 14:17:17 -0400

    Changes discovered in debug mode
    - Fix critical bugs in emboot_unseal and functions. Honestly not sure how this
      ever worked with those bugs: dash was throwing syntax errors.
    
    - Rationalize logging in emboot_unseal. Thinking about adding a
      $LL_DEBUG_DETAIL for the `set -x` in emboot_unseal, because it dumps a *lot*
      of information.

--- emboot_unseal.sh
+++ emboot_unseal.sh
@@ -21,25 +21,25 @@ set -e
 
 if [ "$CRYPTTAB_TRIED" = 0 ]; then
     outcome() {
-        echo "EFI measured boot unseal $1"
-        if [ "$1" != succeeded ] || would_log; then
-            [ -z "${CRYPTTAB_SOURCE}" ] || echo "  backing device: ${CRYPTTAB_SOURCE}"
-            [ -z "${CRYPTTAB_NAME}" ] || echo "  mapped name: ${CRYPTTAB_NAME}"
-            echo "  token ID: $tid"
-            echo "  kernel release: $krel"
-        fi
+        log 'EFI measured boot unseal %s' "$1"
+        local log_prio=$LL_INFO
+        [ "$1" = succeeded ] || log_prio=$LL_ERROR
+        [ -z "${CRYPTTAB_SOURCE}" ] || log -t tpm -l $log_prio 'backing device: %s' "${CRYPTTAB_SOURCE}"
+        [ -z "${CRYPTTAB_NAME}" ] || log -t tpm -l $log_prio 'mapped name: %s' "${CRYPTTAB_NAME}"
+        log -t tpm -l $log_prio 'token ID: %s' "$tid"
+        log -t tpm -l $log_prio 'kernel release: %s' "$krel"
     }
 
     . /etc/efi-measured-boot/config
     [ -n "${cmd##./*}" ] || APPDIR=.
     . "${APPDIR:-/APPDIR-not-set}"/functions
 
-    if would_log 5; then set -x; fi
+    if would_log -t tpm -l $LL_DEBUG; then set -x; fi
 
     tmpdir=$(setup_tmp_dir)
 
-    verbose_do -t tpm -l $LL_DEBUG eval 'read_pcrs >$tmpdir/current_pcrs.txt'
-    verbose_do -t tpm -l $LL_DEBUG eval 'read_counter "$tmpdir"/current_counter'
+    verbose_do -t tpm -l $LL_INFO eval 'read_pcrs >$tmpdir/current_pcrs.txt'
+    verbose_do -t tpm -l $LL_INFO eval 'read_counter "$tmpdir"/current_counter'
 
     krel=$(uname -r)
     for tid in $(list_luks_token_ids "$CRYPTTAB_SOURCE" "$krel"); do
@@ -51,12 +51,12 @@ if [ "$CRYPTTAB_TRIED" = 0 ]; then
         fi
         outcome FAILED
 
-        log_debug -t tpm "counter: current=%d expects<=%d\n" "0x$(xxd -p -c9999 <$tmpdir/current_counter)" "0x$(xxd -p -c9999 <$tmpdir/counter)"
-        verbose_do -t tpm -l $LL_DEBUG eval 'diff_pcrs "$tmpdir"/pcrs "$tmpdir"/current_pcrs.txt | sed -e "s/^/  /"'
+        log_info -t tpm 'counter: current=%d expects<=%d' "0x$(xxd -p -c9999 <$tmpdir/current_counter)" "0x$(xxd -p -c9999 <$tmpdir/counter)"
+        verbose_do -t tpm -l $LL_INFO eval 'diff_pcrs "$tmpdir"/pcrs "$tmpdir"/current_pcrs.txt | sed -e "s/^/  /"'
     done
 
     rm -rf "$tmpdir"
-    echo "Falling back to passphrase entry"
+    log 'Falling back to passphrase entry'
 fi
 
 exec "$keyscript" "$keyscriptarg" >&3 3>&-
