b86864cbc8f9d293d42e086141099efd98dd4e1b
Kyle Rose <krose@krose.org>
2025-09-22 21:22:30 -0400

    Add the proimised LL_DEBUG_DETAIL, and mess with log levels

--- emboot_unseal.sh
+++ emboot_unseal.sh
@@ -24,22 +24,22 @@ if [ "$CRYPTTAB_TRIED" = 0 ]; then
         log 'EFI measured boot unseal %s' "$1"
         local log_prio=$LL_INFO
         [ "$1" = succeeded ] || log_prio=$LL_ERROR
-        [ -z "${CRYPTTAB_SOURCE}" ] || log -t tpm -l $log_prio 'backing device: %s' "${CRYPTTAB_SOURCE}"
-        [ -z "${CRYPTTAB_NAME}" ] || log -t tpm -l $log_prio 'mapped name: %s' "${CRYPTTAB_NAME}"
-        log -t tpm -l $log_prio 'token ID: %s' "$tid"
-        log -t tpm -l $log_prio 'kernel release: %s' "$krel"
+        [ -z "${CRYPTTAB_SOURCE}" ] || log -t boot -l $log_prio 'backing device: %s' "${CRYPTTAB_SOURCE}"
+        [ -z "${CRYPTTAB_NAME}" ] || log -t boot -l $log_prio 'mapped name: %s' "${CRYPTTAB_NAME}"
+        log -t boot -l $log_prio 'token ID: %s' "$tid"
+        log -t boot -l $log_prio 'kernel release: %s' "$krel"
     }
 
     . /etc/efi-measured-boot/config
     [ -n "${cmd##./*}" ] || APPDIR=.
     . "${APPDIR:-/APPDIR-not-set}"/functions
 
-    if would_log -t tpm -l $LL_DEBUG; then set -x; fi
+    if would_log -t tpm,boot -l $LL_DEBUG_DETAIL; then set -x; fi
 
     tmpdir=$(setup_tmp_dir)
 
-    verbose_do -t tpm -l $LL_INFO eval 'read_pcrs >$tmpdir/current_pcrs.txt'
-    verbose_do -t tpm -l $LL_INFO eval 'read_counter "$tmpdir"/current_counter'
+    verbose_do -t tpm,boot -l $LL_INFO eval 'read_pcrs >$tmpdir/current_pcrs.txt'
+    verbose_do -t tpm,boot -l $LL_INFO eval 'read_counter "$tmpdir"/current_counter'
 
     krel=$(uname -r)
     for tid in $(list_luks_token_ids "$CRYPTTAB_SOURCE" "$krel"); do
@@ -51,8 +51,8 @@ if [ "$CRYPTTAB_TRIED" = 0 ]; then
         fi
         outcome FAILED
 
-        log_info -t tpm 'counter: current=%d expects<=%d' "0x$(xxd -p -c9999 <$tmpdir/current_counter)" "0x$(xxd -p -c9999 <$tmpdir/counter)"
-        verbose_do -t tpm -l $LL_INFO eval 'diff_pcrs "$tmpdir"/pcrs "$tmpdir"/current_pcrs.txt | sed -e "s/^/  /"'
+        log_info -t tpm,boot 'counter: current=%d expects<=%d' "0x$(xxd -p -c9999 <$tmpdir/current_counter)" "0x$(xxd -p -c9999 <$tmpdir/counter)"
+        verbose_do -t tpm,boot -l $LL_INFO eval 'diff_pcrs "$tmpdir"/pcrs "$tmpdir"/current_pcrs.txt | sed -e "s/^/  /"'
     done
 
     rm -rf "$tmpdir"
